ee3e7455e3fb7cb360330fad14fdad4db9e56f40
Sim Singh <simsinght@gmail.com>
2020-11-26 22:34:50 -0800

    xuan's work from gameService branch. i just copied it over because merging was being a pain. introduces the models also

---
+++ trouble_server/game/model/database.js
@@ -0,0 +1,23 @@
+const game = require('./game')
+let Game = game.Game
+
+var gameList = new Array()
+var playerList = new Array()
+
+
+
+/**
+ * Hardcode a gamelist
+ */
+
+const testGame = new Game(0)
+testGame.addUser('Jordan', 'blue')
+testGame.addUser('Sam', 'yellow')
+testGame.addUser('Sim', 'red')
+testGame.addUser('Yiwei', 'green')
+gameList.push(testGame)
+module.exports = {
+    //property name: function name
+    gameList: gameList,
+    playerList: playerList
+}
